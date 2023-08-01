bd3106e0597a0938a44e91d7d03869db5940a83b
kathrynb fierro <134311762+yusufozkurt@users.noreply.github.com>
2023-08-01 21:20:48 +0530

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="trendz news" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://trendznews.top">Trendz News</a>
+        </h1>
+
+   
+      </main>
+
+     
+    </div>
+  )
+}
+
+export default Home
